058b81381f9747bbb76e826b30f46d14b179a425
Alima Neff <Dal559433@utdallas.edu>
2026-04-16 16:43:14 -0500

    scratched everything and pasted rain.py code

--- src/paint.py
+++ src/paint.py
@@ -1,13 +1,15 @@
-import turtle
 import random
-import pygame 
+import math
+from tkinter import font
 from numpy import size
+from numpy import size
+import pygame
 
 
-class Circle():
+class Particle():
 
 
-    def __init__(self, pos=(0,0), size =100, life = 1000):
+    def __init__(self, pos=(0,0), size =25, life = 1000):
         #at a specific paremeter, the particle will be created 
         # at a specific position
         # this variable has the property/position of the 
@@ -67,7 +69,7 @@ class ParticleTrail():
         self.particles = []
 
     def update(self, dt):
-        particle = Circle(self.pos, size=self.size, life=self.life)
+        particle = Particle(self.pos, size=self.size, life=self.life)
         self.particles.insert(0, particle) #places particle at the beginning of the list
         self._update_particles(dt)
         self._update_pos()
@@ -79,38 +81,108 @@ class ParticleTrail():
             if particle.dead:
                 del self.particles[idx] #removes the particle from the list if it is dead
 
+    def _update_pos(self):
+        x, y = self.pos
+        y += self.size + 3 #places the next particle below the previous one
+        self.pos = (x,y)
+        #TODO: add some randomness to the x position to make it look more natural, maybe a random offset of -5 to 5 pixels
+        x += random.randint(-2, 2)
+        self.pos = (x,y)
+
+
     def draw(self, surface):
         for particle in self.particles:
             particle.draw(surface)
 
+class Rain():
 
-        
-def main():
-    #display screen
+    def __init__(self, screen_res):
+        self.screen_res = screen_res
+        self.particle_size = 15
+        self.trails = [] #to start keeping track of particle trails
+        self.birth_rate = 1 #trails per frame]
+        self.trails = []
+
+
+    def update(self, dt):
+        self._birth_new_particles()
+        for idx, trail in enumerate(self.trails):
+            trail.update(dt)
+            if self._trail_is_offscreen(trail):
+                del self.trails[idx] #removes the trail from the list if it is offscreen
+
+
+    def _trail_is_offscreen(self, trail):
+        #TODO: check if last particle of trail is offscreen (last on the list)
+        tail_is_offscreen_ = trail.particles[-1].pos[1] > self.screen_res[1]
+        return tail_is_offscreen_
+
+
+    def _birth_new_particles(self):
+        for count in range(self.birth_rate):
+            screen_width = self.screen_res[0]
+            x = random.randrange(0, screen_width, self.particle_size)
+            pos = (x,0)
+            life = random.randrange(500,3000)
+            trail = ParticleTrail(pos, self.particle_size, life)
+            self.trails.insert(0, trail) #places the new trail at the beginning of the list
+
+    def draw(self, surface):
+        for trail in self.trails:
+            trail.draw(surface)
+
+
+
+def main(): 
+    # Initialize Pygame and particles and clock
     pygame.init()
     pygame.display.init()
-    resolution = (800, 600)
+    #find a font I like
+    #print(pygame.font.get_fonts())
+    pygame.font.init()
+    pygame.display.set_caption("Digital Rain")
+    clock = pygame.time.Clock()
+    dt = 0
+    #need to display full screen, so we need to get the screen resolution
+    infoObject = pygame.display.Info() 
+    resolution = (infoObject.current_w, infoObject.current_h)
     screen = pygame.display.set_mode(resolution)
-    circle = Circle(resolution)
+    rain = Rain(resolution)
     running = True
     while running:
         #Event loop
         for event in pygame.event.get():
-            #if user clicks close button, running = false
-            if event.type == pygame.QUIT:
-                running = False
+            #need to add the close button back to window or let user exit with escape key
+            #new and old particles avoid the mouse cursor
+            mouse_pos = pygame.mouse.get_pos()
+            for trail in rain.trails:
+                for particle in trail.particles:
+                    if (abs(particle.pos[0] - mouse_pos[0]) < 50) and (abs(particle.pos[1] - mouse_pos[1]) < 50):
+                        #move the particle away from the mouse cursor by a random offset of 50 pixels
+                        x_offset = random.randint(-50, 50)
+                        y_offset = random.randint(-50, 50)
+                        particle.pos = (particle.pos[0] + x_offset, particle.pos[1] + y_offset)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                elif event.type == pygame.QUIT:
+                    running = False
         #for loop closes
+        # TODO: some game logic
+        rain.update(dt)
         # Render & Display faze
         black = pygame.Color(0,0,0)
         screen.fill(black)
         #pygame.Surface needs coordinates (blit) to appear on screen
-        circle.draw(screen)
+        rain.draw(screen)
         pygame.display.flip()
         #print(particle.age)
+        dt = clock.tick(12) #caps at 1 frames per second
 
 
     # quits the program once run
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()    
